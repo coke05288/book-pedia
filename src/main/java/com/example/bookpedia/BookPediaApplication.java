233a3509365f9ccac860c9045de56d22f2eb5526
YoungJun <coke05288@gmail.com>
2024-08-12 00:38:06 +0900

    initial commit

---
+++ src/main/java/com/example/bookpedia/BookPediaApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookpedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookPediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookPediaApplication.class, args);
+    }
+
+}
